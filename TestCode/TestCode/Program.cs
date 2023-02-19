2c0486f2b5d5042da17f32b9cc413afe9f374dd7
George Litescu <ag_litescu@yahoo.com>
2023-02-19 11:04:00 +0200

    initial commit

---
+++ TestCode/TestCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I should really have learned git by now");
+        }
+    }
+}
