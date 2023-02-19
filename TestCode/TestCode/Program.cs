7c35188dc8e3c44c868a0e285a219ae7e85f3a14
George Litescu <ag_litescu@yahoo.com>
2023-02-19 13:40:07 +0200

    added User class

--- TestCode/TestCode/Program.cs
+++ TestCode/TestCode/Program.cs
@@ -7,6 +7,27 @@ namespace TestCode
         static void Main(string[] args)
         {
             Console.WriteLine("I should really have learned git by now");
+
+            User creator = new User();
+            creator.setName("George");
+            Console.WriteLine("{0} is learning git :o", creator.getName());
+        }
+
+        public class User
+        {
+            private string name;
+
+            public void setName(string name)
+            {
+                this.name = name;
+            }
+
+            public string getName()
+            {
+                string copyOfName = name;
+                return copyOfName;
+            }
+
         }
     }
 }
